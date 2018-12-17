84099330a349e40c17d2db834085754bcad00d4a
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-17 17:40:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end
+
   get '/' do
 
     erb :index
